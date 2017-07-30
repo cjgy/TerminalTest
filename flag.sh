f00dc1071a3481f8e4543a5e54d382368049ea71
cjgy <chrisgilmoreyee@gmail.com>
2017-07-29 20:57:20 -0400

    modified flag.sh to take one argument and show usage on incorrect calls

--- flag.sh
+++ flag.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-#First version of the Flag problem
-#uses set variables, does not take arguments, and does not deal with
-#improper calls
-for ((i=1; i<=5; i++)) do
-	for ((j=1; j<=i; j++)) do
-		printf $i
-	done
-	printf "\n"
-done
+#Second version of the Flag problem
+# takes arguments and deals with improper calls
 
-for ((i=4; i>=1; i--)) do
-	for ((j=i; j>=1; j--)) do
-		printf $i
+
+function show_usage {
+	echo "Usage: $0 flag_width"
+	exit 1
+}
+
+if (($#!=1)); then
+	show_usage
+else
+	for ((i=1;i<=$1;i++)) do
+		for ((j=1;j<=i;j++)) do
+			printf $i
+		done
+		printf "\n"
 	done
-	printf "\n"
-done
+
+	for ((i=$1-1; i>0; i--)) do
+		for ((j=i; j>0; j--)) do
+			printf $i
+		done
+		printf "\n"
+	done
+fi
